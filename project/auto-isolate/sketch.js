34aff88fedb04b4c5bc0858fd89f4cfa4fad62dc
GabeAlexander <43545418+GabeAlexander@users.noreply.github.com>
2020-01-11 16:11:28 -0500

    Isolate on its way

--- project/auto-isolate/sketch.js
+++ project/auto-isolate/sketch.js
@@ -1,28 +1,31 @@
 var video;
 var button;
 var img;
-var counter = 0;
-var vScale = 4;
-var total;
+var img2;
+var varience = 2;
 
 function setup() {
   pixelDensity(1);
-  createCanvas(640, 480);
-  background(51);
   video = createCapture(VIDEO);
-  video.size(320, 240);
+  video.size(640, 480);
+  createCanvas(video.width, video.height);
+  background(0,0,255);
+  button = createButton('Go!');
+  button.mousePressed(drawNew)
   video.mousePressed(takeSnapshot);
   img = video.get();
 }
 
 function takeSnapshot() {
   img = video.get();
+  image(img,0,0,width,height)
 }
 
-function draw() {
-  // image(snapshots[0], 0, 0, 640, 480);
+function drawNew() {
+  clear();
+  img2 = video.get();
   img.loadPixels();
-  // image(img,0,0,640,480)
+  img2.loadPixels();
   for(let x = 0; x <= img.width; x++) {
     for(let y = 0; y <= img.height; y++) {
       var index = (x + y * img.width) * 4;
@@ -30,10 +33,19 @@ function draw() {
       let r = img.pixels[index+0];
       let g = img.pixels[index+1];
       let b = img.pixels[index+2];
+      let r2 = img2.pixels[index+0];
+      let g2 = img2.pixels[index+1];
+      let b2 = img2.pixels[index+2];
+
       noStroke();
-      fill(color(r,g,b));
-      // rectMode(CENTER);
-      rect(x,y,1,1);
+      rectMode(CENTER);
+      fill(r2,g2,b2);
+      if(((r < (r2 - varience)) && (r > (r2 + varience))) && ((g < (g2 - varience)) && (g > (g2 + varience))) || ((b < (b2 - varience)) || (b > (b2 + varience)))) {
+        rect(x,y,1,1);
+      }else {
+        fill(0,0,255)
+        rect(x,y,1,1);
+      }
     }
   }
 }
