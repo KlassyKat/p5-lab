1d6fe70a426fb610b9b70f6aa428e67b374472e0
gabealexander <gabealexander01@gmail.com>
2020-01-11 20:32:02 -0500

    Add more color checks

--- project/auto-isolate/sketch.js
+++ project/auto-isolate/sketch.js
@@ -2,7 +2,7 @@ var video;
 var button;
 var img;
 var img2;
-var varience = 2;
+var varience = 10;
 
 function setup() {
   pixelDensity(1);
@@ -28,7 +28,29 @@ function drawNew() {
   img2.loadPixels();
   for(let x = 0; x <= img.width; x++) {
     for(let y = 0; y <= img.height; y++) {
+      //Get a pixel and all surrounding pixels
       var index = (x + y * img.width) * 4;
+      if (y > 0 && x > 0) {
+        var indexUpLeft = ((x - 1) + (y - 1) * img.width) * 4;
+      }
+      if (y > 0) {
+        var indexUp = (x + (y - 1) * img.width) * 4;
+      }
+      if (y > 0) {
+        var indexUpRight = ((x + 1) + (y - 1) * img.width) * 4;
+      }
+      if (x < img.width) {
+        var indexRight = ((x + 1) + y * img.width) * 4;
+      }
+      if (x < img.width && y < img.height) {
+        var indexDownRight = ((x + 1) + (y + 1) * img.width) * 4;
+      }
+      if (y < img.height) {
+        var indexDown = (x + (y + 1) * img.width) * 4;
+      }
+      if (x > 0 && y < img.height) {
+        var indexDownLeft = ((x - 1) + (y + 1) * img.width) * 4;
+      }
       // var index = (img.width - x + 1 + (y * img.width)) * 4;
       let r = img.pixels[index+0];
       let g = img.pixels[index+1];
@@ -36,14 +58,13 @@ function drawNew() {
       let r2 = img2.pixels[index+0];
       let g2 = img2.pixels[index+1];
       let b2 = img2.pixels[index+2];
-
+      var bright = (r+g+b)/3;
+      var bright2 = (r2+g2+b2)/3;
       noStroke();
       rectMode(CENTER);
       fill(r2,g2,b2);
-      if(((r < (r2 - varience)) && (r > (r2 + varience))) && ((g < (g2 - varience)) && (g > (g2 + varience))) || ((b < (b2 - varience)) || (b > (b2 + varience)))) {
-        rect(x,y,1,1);
-      }else {
-        fill(0,0,255)
+      if(((bright < (bright2 - varience)) || (bright > (bright2 + varience)))) {
+      // if(((r < (r2 - varience)) || (r > (r2 + varience)))) {
         rect(x,y,1,1);
       }
     }
