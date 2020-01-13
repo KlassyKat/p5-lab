de462a80bde795ce76cc5dc76176410b02e59431
GabeAlexander <43545418+GabeAlexander@users.noreply.github.com>
2020-01-13 16:21:35 -0500

    Updated Zone averaging for auto isolate

--- project/auto-isolate/sketch.js
+++ project/auto-isolate/sketch.js
@@ -18,7 +18,9 @@ function setup() {
 
 function takeSnapshot() {
   img = video.get();
-  image(img,0,0,width,height)
+  image(img,0,0,width,height);
+  var test = convertToHex(10,20,30);
+  console.log(test);
 }
 
 function drawNew() {
@@ -30,31 +32,54 @@ function drawNew() {
     for(let y = 0; y <= img.height; y++) {
       //Get a pixel and all surrounding pixels
       var index = (x + y * img.width) * 4;
-      if (y > 0 && x > 0) {
+      if ( y < img.height && y > 0 && x > 0 && x < img.width) {
         var indexUpLeft = ((x - 1) + (y - 1) * img.width) * 4;
-      }
-      if (y > 0) {
         var indexUp = (x + (y - 1) * img.width) * 4;
-      }
-      if (y > 0) {
         var indexUpRight = ((x + 1) + (y - 1) * img.width) * 4;
-      }
-      if (x < img.width) {
         var indexRight = ((x + 1) + y * img.width) * 4;
-      }
-      if (x < img.width && y < img.height) {
         var indexDownRight = ((x + 1) + (y + 1) * img.width) * 4;
-      }
-      if (y < img.height) {
         var indexDown = (x + (y + 1) * img.width) * 4;
-      }
-      if (x > 0 && y < img.height) {
         var indexDownLeft = ((x - 1) + (y + 1) * img.width) * 4;
+        var indexLeft = ((x - 1) + y * img.width) * 4;
+
+        var rUL = img.pixels[indexUpLeft+0];
+        var gUL = img.pixels[indexUpLeft+1];
+        var bUL = img.pixels[indexUpLeft+2];
+
+        var rU = img.pixels[indexUp+0];
+        var gU = img.pixels[indexUp+1];
+        var bU = img.pixels[indexUp+2];
+
+        var rUR = img.pixels[indexUpRight+0];
+        var gUR = img.pixels[indexUpRight+1];
+        var bUR = img.pixels[indexUpRight+2];
+
+        var rR = img.pixels[indexRight+0];
+        var gR = img.pixels[indexRight+1];
+        var bR = img.pixels[indexRight+2];
+
+        var rDR = img.pixels[indexDownRight+0];
+        var gDR = img.pixels[indexDownRight+1];
+        var bDR = img.pixels[indexDownRight+2];
+
+        var rD = img.pixels[indexDown+0];
+        var gD = img.pixels[indexDown+1];
+        var bD = img.pixels[indexDown+2];
+
+        var rDL = img.pixels[indexDownLeft+0];
+        var gDL = img.pixels[indexDownLeft+1];
+        var bDL = img.pixels[indexDownLeft+2];
+
+        var rL = img.pixels[indexLeft+0];
+        var gL = img.pixels[indexLeft+1];
+        var bL = img.pixels[indexLeft+2];
       }
+
       // var index = (img.width - x + 1 + (y * img.width)) * 4;
       let r = img.pixels[index+0];
       let g = img.pixels[index+1];
       let b = img.pixels[index+2];
+
       let r2 = img2.pixels[index+0];
       let g2 = img2.pixels[index+1];
       let b2 = img2.pixels[index+2];
@@ -70,3 +95,18 @@ function drawNew() {
     }
   }
 }
+
+function rgbToHex (rgb) { 
+  var hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+};
+
+function convertToHex(r,g,b) {   
+  var red = rgbToHex(r);
+  var green = rgbToHex(g);
+  var blue = rgbToHex(b);
+  return red+green+blue;
+};
--- project/glitch-video/sketch.js
+++ project/glitch-video/sketch.js
@@ -6,17 +6,18 @@ var vScale = 4;
 var total;
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(1280, 960);
   background(51);
   video = createCapture(VIDEO);
-  video.size(320, 240);
+  video.size(1280, 960);
+  video.hide();
   // button = createButton('snap');
   // button.mousePressed(takesnap);
 }
 
 function draw() {
-  var w = 160;
-  var h = 120;
+  var w = 1280;
+  var h = 960;
   var x = 0;
   var y = 0;
 
@@ -32,7 +33,7 @@ function draw() {
   for (var i = 0; i < snapshots.length; i++) {
     //tint(255, 50);
     var index = (i + frameCount*3) % snapshots.length;//multiplier seems to make glitchier as it goes higher
-    image(snapshots[index], x, y, w, h);
+    image(snapshots[index], 0, 0, 1280, 960);
     x = x + w;
     if (x >= width) {
       x = 0;
